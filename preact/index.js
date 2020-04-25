7bf33687fee8103b206d80a7e8ec74161dcb2e7f
Michael Klein <michael.klein.business@gmail.com>
2020-04-25 16:33:25 +0200

    minor refactors

--- preact/index.js
+++
@@ -1,1 +0,0 @@
-import{createPReactIntegration}from"../src/create_p_react_integration.js";import*as Preact from"../web_modules/htm/preact/standalone.module.js";export const{createComponent:createPreactComponent,layoutEffect,sideEffect}=createPReactIntegration(Preact);export*from"../web_modules/htm/preact/standalone.module.js";
